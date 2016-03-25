0041a1024a47933f63076d86ad81e2678554f2fd
Thomas Cannon <tcannon00@gmail.com>
2016-03-24 21:08:50 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,71 @@
+require 'rubygems'; require 'bundler'; Bundler.require
+
+require 'byebug'
+require 'faraday'
+require 'json'
+
+dir = File.expand_path('..', __FILE__)
+require dir + '/config'
+
+def get_or_post(path, opts={}, &block)
+  get(path, opts, &block)
+  post(path, opts, &block)
+end
+
+get '*' do
+  mock_response
+end
+
+post '*' do
+  mock_response
+end
+
+def mock_response
+  begin
+    #byebug
+    content_type :json
+    # puts "GOT REQUEST"
+    # puts params.inspect
+    # load_response("time.json")
+    # status 200 and return
+    if time?
+      load_response("time.json")
+    elsif response_1?
+      load_response("response-1.json")
+    elsif response_2?
+      load_response("response-2.json")
+    elsif response_3?
+      load_response("response-3.json")
+    else
+      status 404 and return
+    end
+  rescue Exception => e
+    puts e.message
+  end
+end
+
+def load_response(filename, load=true)
+  dir = File.expand_path('..', __FILE__)
+  puts "SENDING #{filename}"
+  if load
+    x = File.read(File.join(dir, filename))
+    # puts x
+    x
+  end
+end
+
+def time?
+  params.include? "v.missionTime"
+end
+
+def response_1?
+  params.include? "o.maneuverNodes"
+end
+
+def response_2?
+	params.include? "currentReferenceBodyRadius"
+end
+
+def response_3?
+	params.include? "vesselCurrentPositionRelativePosition"
+end
---
+++ config.rb
@@ -0,0 +1,14 @@
+CLIENT_ID = "4lxf6g8141vcu0rqgx0k5huys"
+CLIENT_SECRET = "e14pqdat0nbfru3t27p1xnx3p"
+PROVIDER_SITE = "http://secure.freckle.dev"
+AUTHORIZE_URL = '/oauth/2/authorize'
+TOKEN_URL = '/oauth/2/access_token'
+REDIRECT_URI = "http://localhost:4567/step2"
+
+
+# CLIENT_ID = "9978a4c757bf028bf659"
+# CLIENT_SECRET = "661a537a29a2c3139edec755228b761bc7ba06bd"
+# PROVIDER_SITE = "https://github.com/"
+# AUTHORIZE_URL = '/login/oauth/authorize'
+# TOKEN_URL = '/login/oauth/access_token'
+# REDIRECT_URI = "http://localhost:4567/step2"
